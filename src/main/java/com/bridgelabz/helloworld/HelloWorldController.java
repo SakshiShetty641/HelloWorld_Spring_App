87988f5a0fe3f65e7df2a519c58bf8cf17e3b33a
Sakshi <sakshishetty1231997@gmail.com>
2021-10-07 23:22:09 +0530

    Added: To display the output using RequestParam

--- src/main/java/com/bridgelabz/helloworld/HelloWorldController.java
+++ src/main/java/com/bridgelabz/helloworld/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.helloworld;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +8,15 @@ public class HelloWorldController {
     @PostMapping("/greet")
     public String getMessage(){
         return  "Hello World";
+    }
+
+    @RequestMapping(value = {"/", "/hello", "message"})
+    public String getHelloMessage(){
+        return "Hello BridgeLabz";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test1(@RequestParam String fname, @RequestParam String lname){
+        return "Hello : " + fname + " " + lname;
     }
 }
